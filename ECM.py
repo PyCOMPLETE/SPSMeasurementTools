c0699d16a2dfc7d751e4096033bec06bb0e9ce5e
giadarol <giovanni.iadarola@cern.ch>
2021-04-15 15:28:19 +0200

    2to32to32to3

--- ECM.py
+++ ECM.py
@@ -1,4 +1,4 @@
-from sddsdata import sddsdata
+from .sddsdata import sddsdata
 import numpy as np
 import scipy.io as sio
 import os
@@ -7,8 +7,8 @@ import gzip
 import time
 import glob
 import pickle
-from zip_mat import save_zip
-import timestamp_helpers as th
+from .zip_mat import save_zip
+from . import timestamp_helpers as th
 
 
 class ECLDMON:
@@ -27,8 +27,8 @@ class ECLDMON:
         elif complete_path.endswith('.mat'):
             dict_ecld = sio.loadmat(complete_path)
         else:
-            print('Unknown file extension for ECLDMON file. Should be ' +
-                  '.mat or .mat.gz')
+            print(('Unknown file extension for ECLDMON file. Should be ' +
+                  '.mat or .mat.gz'))
 
         ecld_mon_rescaled = dict_ecld['eclm_data']
 
@@ -73,35 +73,35 @@ class ECLDMON:
 def make_pickle(start_from_last=True, pickle_name_ecm='ecm_overview.pkl', pickle_name_bct='bct_overview.pkl', mat_folder='ecm/', mat_file_prefix='SPSmeas_', t_obs_1st_inj=1.5, n_points_to_cut=10, SPSuser_blacklist = []):
 
     with open(pickle_name_bct) as fid:
-	    beams_bct = pickle.load(fid)
-    
+            beams_bct = pickle.load(fid)
+
     semcloud_lists = get_ecm_tstamps_from_filenames(mat_folder, SPSuser_blacklist, beams_bct)
 
     try:
         with open(pickle_name_ecm) as fid:
-	        ecm_dict = pickle.load(fid)
+                ecm_dict = pickle.load(fid)
     except IOError:
-        print "I CREATE (!) the ecm pickle file"
+        print("I CREATE (!) the ecm pickle file")
         ecm_dict = {'1a':{}, '1b':{}, '2a':{}, '2b':{}}
 
-    
+
     for ecm_id in '1a 1b 2a 2b'.split():
 
         ecm_device = int(ecm_id[0])
         ecm_part = ecm_id[-1]
-            
+
         beams_ecm = ecm_dict[ecm_id]
 
-        for SPSuser in beams_bct.keys():
+        for SPSuser in list(beams_bct.keys()):
             if SPSuser in SPSuser_blacklist:
                continue
 
             t_stamps_ecm = np.array(semcloud_lists[ecm_device][SPSuser])
             if len(t_stamps_ecm) == 0:
-                print 'No ECM data for user: %s'%SPSuser
+                print('No ECM data for user: %s'%SPSuser)
                 continue
 
-            if not(SPSuser in beams_ecm.keys()):
+            if not(SPSuser in list(beams_ecm.keys())):
                 beams_ecm[SPSuser] = {}
                 beams_ecm[SPSuser]['timestamp_bct'] = []
                 beams_ecm[SPSuser]['timestamp_ecm'] = []
@@ -112,7 +112,7 @@ def make_pickle(start_from_last=True, pickle_name_ecm='ecm_overview.pkl', pickle
                 beams_ecm[SPSuser]['gain_sigma'] = []
 
             N_cycles = len(beams_bct[SPSuser]['timestamp_float'])
-		    
+
             for ii in range(N_cycles):
 
                 t_stamp = beams_bct[SPSuser]['timestamp_float'][ii]
@@ -121,46 +121,45 @@ def make_pickle(start_from_last=True, pickle_name_ecm='ecm_overview.pkl', pickle
                         continue
                 elif t_stamp in beams_ecm[SPSuser]['timestamp_bct']:
                     continue
-                print '%s %s %d/%d'%(ecm_id, SPSuser, ii, N_cycles - 1)
+                print('%s %s %d/%d'%(ecm_id, SPSuser, ii, N_cycles - 1))
                 idx_nearest = np.argmin(np.abs(t_stamps_ecm - t_stamp))
                 difference = t_stamps_ecm[idx_nearest] - t_stamp
-                print 'difference %s s'%difference
+                print('difference %s s'%difference)
 
                 found = False
                 for jj in [0,1,-1,2,-2,3,-3,4,-4,5,-5,6,-6,7,-7]:
                     if (idx_nearest+jj > 0) and (idx_nearest+jj) < len(t_stamps_ecm):
-	                    filename_ecm = mat_file_prefix + SPSuser + '_SEMCLOUD%d'%int(ecm_device) + ('_%d.mat.gz'%t_stamps_ecm[idx_nearest+jj])
+                            filename_ecm = mat_file_prefix + SPSuser + '_SEMCLOUD%d'%int(ecm_device) + ('_%d.mat.gz'%t_stamps_ecm[idx_nearest+jj])
 
-	                    curr_ecm = ECLDMON(mat_folder + '%s/SEMCLOUD%d/'%(SPSuser, ecm_device) + filename_ecm, ecm_part)
-	                    print curr_ecm.SC_numb, beams_bct[SPSuser]['SC_numb_vect'][ii]
-	                    if curr_ecm.SC_numb == beams_bct[SPSuser]['SC_numb_vect'][ii]:
-		                    beams_ecm[SPSuser]['ecld_1st_inj'].append(curr_ecm.total_at_time(t_obs_1st_inj))
-		                    beams_ecm[SPSuser]['int_time'].append(curr_ecm.integ_time)
-		                    beams_ecm[SPSuser]['gain_av'].append(np.mean(curr_ecm.gain3))
-		                    beams_ecm[SPSuser]['gain_sigma'].append(np.mean(curr_ecm.gain3))
-		                    beams_ecm[SPSuser]['ecld_max'].append(np.min(curr_ecm.total_vs_time()[:-n_points_to_cut]))
-		                    beams_ecm[SPSuser]['timestamp_bct'].append(t_stamp)
-		                    beams_ecm[SPSuser]['timestamp_ecm'].append(t_stamps_ecm[idx_nearest+jj])
-		                    print 'OK', jj
-		                    found = True
-		                    break
+                            curr_ecm = ECLDMON(mat_folder + '%s/SEMCLOUD%d/'%(SPSuser, ecm_device) + filename_ecm, ecm_part)
+                            print(curr_ecm.SC_numb, beams_bct[SPSuser]['SC_numb_vect'][ii])
+                            if curr_ecm.SC_numb == beams_bct[SPSuser]['SC_numb_vect'][ii]:
+                                    beams_ecm[SPSuser]['ecld_1st_inj'].append(curr_ecm.total_at_time(t_obs_1st_inj))
+                                    beams_ecm[SPSuser]['int_time'].append(curr_ecm.integ_time)
+                                    beams_ecm[SPSuser]['gain_av'].append(np.mean(curr_ecm.gain3))
+                                    beams_ecm[SPSuser]['gain_sigma'].append(np.mean(curr_ecm.gain3))
+                                    beams_ecm[SPSuser]['ecld_max'].append(np.min(curr_ecm.total_vs_time()[:-n_points_to_cut]))
+                                    beams_ecm[SPSuser]['timestamp_bct'].append(t_stamp)
+                                    beams_ecm[SPSuser]['timestamp_ecm'].append(t_stamps_ecm[idx_nearest+jj])
+                                    print('OK', jj)
+                                    found = True
+                                    break
                 if not found:
-                    print 'Nofile', t_stamp
+                    print('Nofile', t_stamp)
 
             ind_sorted = np.argsort(beams_ecm[SPSuser]['timestamp_bct'])
-            for kk in beams_ecm[SPSuser].keys():
+            for kk in list(beams_ecm[SPSuser].keys()):
                 beams_ecm[SPSuser][kk] = list(np.take(beams_ecm[SPSuser][kk], ind_sorted))
 
             with open(pickle_name_ecm, 'w') as fid:
                 pickle.dump(ecm_dict,fid)
 
 
-           
-def get_ecm_tstamps_from_filenames(ecm_mat_folder, SPSuser_blacklist, beams_bct):            
-    
+def get_ecm_tstamps_from_filenames(ecm_mat_folder, SPSuser_blacklist, beams_bct):
+
     semcloud_lists = {1:{},2:{}}
 
-    for SPSuser in beams_bct.keys():
+    for SPSuser in list(beams_bct.keys()):
         if SPSuser in SPSuser_blacklist:
           continue
 
@@ -173,20 +172,20 @@ def get_ecm_tstamps_from_filenames(ecm_mat_folder, SPSuser_blacklist, beams_bct)
                     semcloud_lists[ecm_device][SPSuser].append(t_stamp_curr)
 
             except OSError as err:
-                print err
+                print(err)
 
-            semcloud_lists[ecm_device][SPSuser] = np.sort(semcloud_lists[ecm_device][SPSuser])            
+            semcloud_lists[ecm_device][SPSuser] = np.sort(semcloud_lists[ecm_device][SPSuser])
 
     return semcloud_lists
 
 
 def sdds_to_dict(in_complete_path):
-	
+
     try:
-	    temp = sddsdata(in_complete_path, endian='little', full=True)
+            temp = sddsdata(in_complete_path, endian='little', full=True)
     except IndexError:
-	    print 'Failed to open data file. (save_bct_mat)'
-	    return
+            print('Failed to open data file. (save_bct_mat)')
+            return
     data = temp.data[0]
 
     acq_comment = data['ACQ-COMMENT'].tostring()
@@ -244,34 +243,34 @@ def sdds_to_file(in_complete_path, mat_filename_prefix='SPSmeas_', outp_folder='
     out_complete_path = out_directory +'/'+ out_filename
 
     if not os.path.isdir(out_directory):
-	    print 'I create folder: '+ out_directory
-	    os.makedirs(out_directory)
+            print('I create folder: '+ out_directory)
+            os.makedirs(out_directory)
 
     sio.savemat(out_complete_path, dict_meas, oned_as='row')
     save_zip(out_complete_path)
 
 
 def make_mat_files(start_time, end_time='Now', data_folder='/user/slops/data/SPS_DATA/OP_DATA/SEM_CLOUD/',
-		  SPSuser=None, filename_converted='ecm_converted.txt'):
+                  SPSuser=None, filename_converted='ecm_converted.txt'):
 
     if type(start_time) is str:
-	    start_tstamp_unix = th.localtime2unixstamp(start_time)
+            start_tstamp_unix = th.localtime2unixstamp(start_time)
     if end_time == 'Now':
-	    end_tstamp_unix = time.mktime(time.localtime())
+            end_tstamp_unix = time.mktime(time.localtime())
     elif type(end_time) is str:
-	    end_tstamp_unix = th.localtime2unixstamp(end_time)
+            end_tstamp_unix = th.localtime2unixstamp(end_time)
 
     try:
-	    with open(filename_converted, 'r') as fid:
-		    list_converted = fid.read().split('\n')
+            with open(filename_converted, 'r') as fid:
+                    list_converted = fid.read().split('\n')
     except IOError:
-	    list_converted = []
+            list_converted = []
 
 
-    print '\nConverting data in folder: %s\n'%data_folder
+    print('\nConverting data in folder: %s\n'%data_folder)
     file_list = glob.glob(data_folder + '*.sdds*')
     for filename in file_list:
-        
+
         complete_path = filename
 
         t_str_sdds = complete_path.split('.sdds')[0].split('_')[-2] +' '+ complete_path.split('.sdds')[0].split('_')[-1]
@@ -282,22 +281,22 @@ def make_mat_files(start_time, end_time='Now', data_folder='/user/slops/data/SPS
         if SPSuser != None:
             user_filename = filename.split('.')[-2]
             if user_filename != SPSuser:
-	            continue
+                    continue
 
         if filename in list_converted:
             continue
 
         try:
-            print complete_path
+            print(complete_path)
 
             sdds_to_file(complete_path)
             with open(filename_converted, 'a+') as fid:
-	            fid.write(filename+'\n')
+                    fid.write(filename+'\n')
         except Exception as err:
-            print 'Skipped:'
-            print complete_path
-            print 'Got exception:'
-            print err
+            print('Skipped:')
+            print(complete_path)
+            print('Got exception:')
+            print(err)
 
 
 
